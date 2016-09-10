ec5ab1398960212cfd8e8d66251e8f6fc242f287
Rick Van Rousselt <rick@rivaro.be>
2016-09-10 11:22:56 +0200

    Updated env variables

--- DataExtracter/app.js
+++ DataExtracter/app.js
@@ -1,8 +1,8 @@
 ﻿var azure = require('azure-storage');
 
-var queueSvc = azure.createQueueService('twitstream', '525tGSMvxz9I4mir3ucxqmdaUJTEjNLVNC9UIyVWYudHrU3jU2Pi7YCYLOVcoWj6YyI08DW8SGp8CrVpIrF2iQ==');
+var queueSvc = azure.createQueueService('twitstream', process.env.azure_key);
 
-var tableSvc = azure.createTableService('twitstream', '525tGSMvxz9I4mir3ucxqmdaUJTEjNLVNC9UIyVWYudHrU3jU2Pi7YCYLOVcoWj6YyI08DW8SGp8CrVpIrF2iQ==');
+var tableSvc = azure.createTableService('twitstream', process.env.azure_key);
 
 function convertJsonToTask(json, partitionKey, rowKey) {
     var jsonResult = JSON.parse(json); 
@@ -13,10 +13,21 @@ function convertJsonToTask(json, partitionKey, rowKey) {
         RowKey: entGen.String(rowKey),
         description: entGen.String(json),
         username: entGen.String(jsonResult.user.name),
+        screen_name: entGen.String(jsonResult.user.screen_name),
+        location: entGen.String(jsonResult.user.location),
+        friends_count: entGen.String(jsonResult.user.friends_count),
+        lang: entGen.String(jsonResult.user.lang),
+        profile_background_image_url: entGen.String(jsonResult.user.profile_background_image_url),
         profileimage: entGen.String(jsonResult.user.profile_image_url),
         tweet_text: entGen.String(jsonResult.text),
         source: entGen.String(jsonResult.source),
         followers: entGen.String(jsonResult.user.followers_count),
+        hashtags: entGen.String(jsonResult.entities.hashtags),
+        media_url: entGen.String(jsonResult.entities.media.media_url),
+        media_type: entGen.String(jsonResult.entities.media.type),
+        tex: entGen.String(jsonResult.text),
+		created_at: entGen.String(jsonResult.created_at)
+
     };
     return task;
 };
@@ -29,20 +40,20 @@ function main() {
             var message = result[0];
             if (message != null) {
                 var queryArray = message.messageText.split(';')
-                //var query = new azure.TableQuery().where('PartitionKey eq ?', queryArray[0]).and('RowKey eq ?', queryArray[1]);
+                
                 tableSvc.retrieveEntity('incomingstreamcontents', queryArray[0], queryArray[1], function (error, result, response) {
                     if (!error) {
                     // query was successful
                     }
-                    
-                    var task = convertJsonToTask(result.description._, result.PartitionKey._, result.RowKey._)
+					
+                    var task = convertJsonToTask(result.description._, result.PartitionKey._, result.RowKey._);
                     
                     tableSvc.replaceEntity('incomingstreamcontents', task, function (error, result, response) {
                         if (!error) {
                             // Entity updated
                             queueSvc.createQueueIfNotExists("processedstreamids", function (error, result, response) {
                                 if (!error) {
-                                    var queMessage = message.messageText + ";" + task.profileimage._;
+                                    var queMessage = message.messageText + "|" + task.profileimage._;
                                     queueSvc.createMessage('processedstreamids', queMessage , function (error, result, response) {
                                         if (!error) {
             
